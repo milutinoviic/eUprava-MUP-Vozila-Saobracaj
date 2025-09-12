7fbd73f1f22503a4fca17528aece3e2a7b657906
Stefan <milutinovic.stefan2003@gmail.com>
2025-09-12 20:14:29 +0200

    add microservices mup-vehicles

---
+++ BACK/mup-vehicles/src/main/java/com/example/mupvehicles/MupVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.example.mupvehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MupVehiclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MupVehiclesApplication.class, args);
+    }
+
+}
---
+++ BACK/mup-vehicles/src/test/java/com/example/mupvehicles/MupVehiclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mupvehicles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MupVehiclesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
